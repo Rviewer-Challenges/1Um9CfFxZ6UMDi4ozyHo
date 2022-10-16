ad59aecd7535253b7a98c1f5321b13ec0692a1ce
Eduardo Peláez <eapelaezc@correo.url.edu.gt>
2022-10-15 20:47:08 -0600

    Initialize project
    Initialize MVC architecture and add some assets

---
+++ unitconverter/unitconverter/Model/Converter.swift
@@ -0,0 +1,16 @@
+//
+//  Converter.swift
+//  unitconverter
+//
+//  Created by Eduardo Pelaez on 15/10/22.
+//
+
+import Foundation
+
+struct Converter {
+    var fromUnit:String = ""
+    var toUnit:String = ""
+    var category:String = ""
+    var result:String = ""
+    
+}
